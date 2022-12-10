e2136023adfe18f8d4d7efe5aa9f0a528039212c
HaroldBenoit <harold_benoit@hotmail.ch>
2022-12-10 18:42:29 +0100

    refactor data module

--- code/training.py
+++ code/training.py
@@ -1,4 +1,4 @@
-from smiles_lightning_data_module import UcrDataModule
+from code.ucr_lightning_data_module import UcrDataModule
 from lightning_model import LightningClassicGNN
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import WandbLogger
