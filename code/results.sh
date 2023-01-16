4e1e0b87625978738e09c2b2ad957294a2cd84ec
hbenoit@lts2gdk0 <hbenoit@lts2gdk0.epfl>
2023-01-16 17:17:33 +0100

    hydrogen comparison

--- code/results.sh
+++ code/results.sh
@@ -1,30 +1,32 @@
-#for i in "bbbp p_np" "freesolv y" "bace Class" "bace pIC50" 
-#do
-#    set -- $i # converts the "tuple" into the pram args $1 $2
-#    for seed in 100 200 300
-#        do
-#
-#        python3 training.py --no_log --results --cluster --hydrogen --root ../data/$1 --target $2 --dataset $1 --train_frac 0.8 --valid_frac 0.2 --test_frac 0.0 --seed $seed --num_message_layers 3 --scaffold --epochs 100
-#        sleep 15s
-#
-#        done
-#
-#done
-
-
-
-for i in "phonons phonons" "dieletric n" "log_gvrh log_gvrh" "perovskites e_form" 
+for i in "bbbp p_np" "freesolv y" "bace Class" "bace pIC50" 
 do
     set -- $i # converts the "tuple" into the pram args $1 $2
     for seed in 100 200 300
         do
 
-        python3 training.py --no_log --results --cluster --root ../data/matbench/$1 --target $2 --dataset $1 --train_frac 0.8 --valid_frac 0.2 --test_frac 0.0 --seed $seed --num_message_layers 3 --epochs 100 --boolean
+        python3 training.py --no_log --results --cluster --root ../data/$1 --target $2 --dataset $1 --train_frac 0.8 --valid_frac 0.2 --test_frac 0.0 --seed $seed --num_message_layers 3 --scaffold --epochs 100
         sleep 15s
 
         done
 
 done
+
+## ADD BACK HYDROGEN
+
+
+
+#for i in "phonons phonons" "dieletric n" "log_gvrh log_gvrh" "perovskites e_form" 
+#do
+#    set -- $i # converts the "tuple" into the pram args $1 $2
+#    for seed in 100 200 300
+#        do
+#
+#        python3 training.py --no_log --results --cluster --root ../data/matbench/$1 --target $2 --dataset $1 --train_frac 0.8 --valid_frac 0.2 --test_frac 0.0 --seed $seed --num_message_layers 3 --epochs 100 --boolean
+#        sleep 15s
+#
+#        done
+#
+#done
 
 ## maybe try boolean ?
 
